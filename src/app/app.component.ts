3679f1b10b1931d8df80823fd677cbec0c8e6cff
Kishan Patel <kolipatelkishan786@gmail.com>
2019-03-25 07:29:41 +0530

    359 Advanced Transitions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,40 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   animations: [
     trigger('divState', [
       state('normal', style({
-        'backgroundColor': 'red',
+        'background-color': 'red',
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
-        'backgroundColor': 'blue',
+        'background-color': 'blue',
         transform: 'translate(100px)'
       })),
-      transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('normal <=> highlighted', animate(300))
+      // transition('highlighted => normal', animate(800))
 
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translate(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translate(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
+
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
@@ -35,9 +54,10 @@ export class AppComponent {
 
   onAnimation() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
   }
 
   onShink() {
-
+     this.wildState = 'shrunken';
   }
 }
